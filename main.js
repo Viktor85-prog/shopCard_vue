72fcbeac39964a413e90aead207e8adc6116a83c
viktor <viktor.bulkin@stremline-c.com>
2022-08-01 17:14:51 +0300

    event handling

--- main.js
+++ main.js
@@ -2,16 +2,27 @@ var product = new Vue({
     el: '#app',
     data: {
         product: "Star",
-        image: './assets/star.jpg',
+        image: './assets/star_gold.jpg',
         inventory: 8,
-        inStock:false,
-        details:["60% gold", '40% metal', 'gender - female'],
+        inStock: false,
+        details: ["60% gold", '40% metal', 'gender - female'],
         variants: [{
-            variantId:999,
-            variantColor: 'gold'
+            variantId: 999,
+            variantColor: 'gold',
+            variantImage: './assets/star_gold.jpg'
         }, {
-            variantId:555,
-            variantColor: 'yellow'
-        }]
+            variantId: 555,
+            variantColor: 'yellow',
+            variantImage: './assets/star_yellow.jpg'
+        }],
+        cart: 0
+    },
+    methods: {
+        addToCart () {
+            this.cart += 1
+        },
+        updateProduct (variantImage) {
+            this.image = variantImage
+        }
     }
 })
