b011cfe58d316939d15f65060a540af374b91f47
viktor <viktor.bulkin@stremline-c.com>
2022-08-01 16:51:34 +0300

    list rendering

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ var product = new Vue({
         product: "Star",
         image: './assets/star.jpg',
         inventory: 8,
-        inStock:false
-
+        inStock:false,
+        details:["60% gold", '40% metal', 'gender - female'],
+        variants: [{
+            variantId:999,
+            variantColor: 'gold'
+        }, {
+            variantId:555,
+            variantColor: 'yellow'
+        }]
     }
 })
