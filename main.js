f26ef7a605d195327c03389b693f20e8e76893ff
viktor <viktor.bulkin@stremline-c.com>
2022-08-02 17:30:32 +0300

    computed properties

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 var product = new Vue({
     el: '#app',
     data: {
+        brand: "XAM",
         product: "Star",
-        image: './assets/star_gold.jpg',
-        inventory: 8,
-        inStock: false,
+        selectedVariant: 0,
         details: ["60% gold", '40% metal', 'gender - female'],
         variants: [{
             variantId: 999,
             variantColor: 'gold',
             variantImage: './assets/star_gold.jpg',
-            fz: '15px'
+            fz: '15px',
+            variantQuantity: 0
         }, {
             variantId: 555,
             variantColor: 'yellow',
             variantImage: './assets/star_yellow.jpg',
-            fz: '10px'
+            fz: '10px',
+            variantQuantity: 15
         }],
         cart: 0
     },
@@ -23,8 +24,19 @@ var product = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return `${this.brand}  ${this.product}`
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
